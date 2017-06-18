348f3a556a58e11188a6f7aa6f4014d46e11abc5
Chris Ha <chunghha@users.noreply.github.com>
2017-06-18 01:36:43 -0500

    Load Webfont

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-###Learn Phaser, code in TypeScript.
+### Learn Phaser, code in TypeScript.
 
 
 1. Project Setup / Display Phaser.Text.
 2. Set Stage background color.
 3. Change alpha of text by J/K keys.
+4. Load Webfont.
 
 
 *Credit to*
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as Phaser from 'phaser-ce';
+import * as WebFont from 'webfontloader';
 
 import {decreaseTextAlpha, increaseTextAlpha, setStageBackgroundColor} from './util';
 
@@ -7,33 +8,37 @@ class Main {
   text: Phaser.Text;
 
   constructor() {
+    WebFont.load({google: {families: ['Signika']}});
+
     this.game = new Phaser.Game(
         800, 600, Phaser.AUTO, 'content', {create: this.create});
   }
 
   create() {
-    const hello = 'Hello World!';
-    const style = {
-      font: '65px Sans Serif',
-      fill: '#d7e7a7',
-      boundsAlignH: 'center',
-      boundsAlignV: 'middle'
-    };
-    this.text = this.game.add.text(0, 0, hello, style);
-    this.text.setTextBounds(0, 0, 800, 600);
+    this.game.time.events.add(Phaser.Timer.SECOND, () => {
+      const hello = 'Hello World!';
+      const style = {
+        font: '96px Signika',
+        fill: '#d7e7a7',
+        boundsAlignH: 'center',
+        boundsAlignV: 'middle'
+      };
+      this.text = this.game.add.text(0, 0, hello, style);
+      this.text.setTextBounds(0, 0, 800, 600);
+
+      // change alpha of text by J/K keys.
+      this.text.alpha = 1;
+      this.game.input.keyboard.addKey(Phaser.Keyboard.J)
+          .onDown.add(text => decreaseTextAlpha(this.text), this);
+      this.game.input.keyboard.addKey(Phaser.Keyboard.K)
+          .onDown.add(text => increaseTextAlpha(this.text), this);
+    });
 
     // initial background color with space cadet.
     this.game.stage.backgroundColor = '#1d2951';
     // add pointer down listener to set stage background color.
     this.game.input.onDown.add(
         stage => setStageBackgroundColor(this.game.stage), this);
-
-    // change alpha of text by J/K keys.
-    this.text.alpha = 1;
-    this.game.input.keyboard.addKey(Phaser.Keyboard.J)
-        .onDown.add(text => decreaseTextAlpha(this.text), this);
-    this.game.input.keyboard.addKey(Phaser.Keyboard.K)
-        .onDown.add(text => increaseTextAlpha(this.text), this);
   }
 }
 
